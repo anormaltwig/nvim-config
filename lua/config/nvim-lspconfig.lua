5408836270294ba5cc7084d0928c4b17d9f074c8
ANormalTwig <1andonlytwig@gmail.com>
2023-06-02 14:32:00 -0400

    lspconfig update

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -1,21 +1,22 @@
--- Lua
 local luasnip = require("luasnip")
 
 local cmp = require("cmp")
 cmp.setup({
-	snippet = {},
-	window = {},
-	mapping = cmp.mapping.preset.insert({
-		["<C-Space>"] = cmp.mapping.confirm(),
-	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 	}, {
 		{ name = "buffer" },
 	}),
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.confirm(),
+	}),
+	snippet = {
+		expand = function(a) return luasnip.lsp_expand(a.body) end,
+	},
 })
 
+-- Lua
 local lspconfig = require("lspconfig")
 lspconfig.lua_ls.setup({
 	settings = {
@@ -36,13 +37,11 @@ lspconfig.lua_ls.setup({
 })
 
 -- C/C++
-
 lspconfig.clangd.setup({
 	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
 -- TypeScript
-
 lspconfig.tsserver.setup({
 	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
