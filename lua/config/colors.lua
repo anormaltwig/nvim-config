ecb2ca9e5d6b4c8a2a716e606b43ebe41cb86801
ANormalTwig <1andonlytwig@gmail.com>
2024-06-03 18:38:56 -0400

    new colors

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -1,19 +1,24 @@
 local colors = {
 	CursorLine = { bg = "#241443" },
 	NonText = { fg = "#112233" },
-	LspInlayHint = { fg = "#888888" },
 
 	NormalFloat = { bg = "NONE", fg = "#ffffff" },
 	FloatBorder = { fg = "#ff55ff" },
 
 	Normal = { bg = "NONE", fg = "#ffffff" },
-	Constant = { fg = "#fc8383" },
+	Constant = { fg = "#aa55ff" },
 	Statement = { bold = true, fg = "#7777ff" },
-
 	PreProc = { fg = "#ff3388" },
 	Type = { bold = true, fg = "#44ffaa" },
-
 	Special = { fg = "#ee55aa" },
+
+	-- LSP Messages
+	LspInlayHint = { fg = "#888888" },
+	DiagnosticError = { fg = "#ff4433" },
+	DiagnosticWarn = { fg = "#ffbb22" },
+	DiagnosticInfo = { fg = "#88ddff" },
+	DiagnosticHint = { fg = "#bbbbbb" },
+	DiagnosticOk = { fg = "#22ff22" },
 
 	Comment = { fg = "#888888" },
 	["@variable"] = { fg = "#42f4f4" },
